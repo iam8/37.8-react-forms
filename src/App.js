4e04c70d8603f3b9ccee3edef285cd6d439d297a
alexm <alex-m@outlook.com>
2023-07-20 17:14:26 -0700

    Render a BoxList element

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@
  */
 
 import React from "react";
+import BoxList from "./BoxList";
 import "./App.css";
 
 
 function App() {
     return (
         <div className="App">
-
+            <BoxList bkgColor="salmon" width={200} height={200} />
         </div>
     );
 }
