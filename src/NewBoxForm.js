45785e1df33194ca636c28c53b2080a7d2c664e6
alexm <alex-m@outlook.com>
2023-07-20 19:02:47 -0700

    Write and use handler for form submissions; add onChange handler to each form input

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -5,7 +5,7 @@ import React, {useState} from "react";
  * NewBoxForm - renders a form that creates a new Box with the submitted width, height, and
  * background color.
  */
-function NewBoxForm() {
+function NewBoxForm({addBox}) {
     const INITIAL_FORM = {
         bkgColor: "",
         width: "",
@@ -24,22 +24,28 @@ function NewBoxForm() {
         });
     };
 
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        addBox(formData);
+        setFormData(INITIAL_FORM);
+    };
+
     return (
         <div className="NewBoxForm">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="bkg-color">Background color:</label>
-                <input id="bkg-color" name="bkgColor" type="text"/>
+                <input id="bkg-color" name="bkgColor" type="text" onChange={handleChange}/>
 
                 <label htmlFor="width">Width:</label>
-                <input id="width" name="width" type="text"/>
+                <input id="width" name="width" type="text" onChange={handleChange}/>
 
                 <label htmlFor="height">height:</label>
-                <input id="height" name="height" type="text"/>
+                <input id="height" name="height" type="text" onChange={handleChange}/>
 
                 <button>Add new box!</button>
             </form>
         </div>
-    )
+    );
 }
 
 
