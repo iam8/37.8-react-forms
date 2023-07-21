e3e7113f180cc73aee5c639c397bdb76e38376a5
alexm <alex-m@outlook.com>
2023-07-20 18:04:42 -0700

    Create initial NewBoxForm component and basic documentation - not yet filled in

---
+++ src/NewBoxForm.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+
+/**
+ * NewBoxForm - renders a form that creates a new Box with the submitted width, height, and
+ * background color.
+ */
+function NewBoxForm() {
+
+}
+
+
+export default NewBoxForm;
