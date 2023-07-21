be38f1009bc0bd19d4c3f921e652a0ea3a064286
alexm <alex-m@outlook.com>
2023-07-20 18:16:29 -0700

    Render basic form with required inputs and submit button

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -7,6 +7,22 @@ import React, {useState} from "react";
  */
 function NewBoxForm() {
 
+    return (
+        <div className="NewBoxForm">
+            <form>
+                <label for="bkg-color">Background color:</label>
+                <input id="bkg-color" type="text"/>
+
+                <label for="width">Width:</label>
+                <input id="width" type="text"/>
+
+                <label for="height">height:</label>
+                <input id="height" type="text"/>
+
+                <button>Add new box!</button>
+            </form>
+        </div>
+    )
 }
 
 
