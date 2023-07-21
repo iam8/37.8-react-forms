cc43e5c739e9056ee0ec3f810992b0d4f7df5e77
alexm <alex-m@outlook.com>
2023-07-20 18:07:58 -0700

    Add second box to list

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <BoxList boxList={[{bkgColor: "salmon", width: 200, height: 200}]} />
+            <BoxList boxList={[
+                {bkgColor: "salmon", width: 200, height: 200},
+                {bkgColor: "red", width: 100, height: 400},
+            ]} />
         </div>
     );
 }
