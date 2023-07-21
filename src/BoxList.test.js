be805c20f279adc83ad1846c531699bf3cd2a12f
alexm <alex-m@outlook.com>
2023-07-20 17:20:01 -0700

    Add snapshot test

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -5,3 +5,9 @@ import BoxList from './BoxList';
 test("Renders without crashing", () => {
     render(<BoxList />);
 });
+
+
+test("Matches snapshot for a single box rendered", () => {
+    const {asFragment} = render(<BoxList bkgColor="salmon" width={200} height={200} />);
+    expect(asFragment).toMatchSnapshot();
+});
