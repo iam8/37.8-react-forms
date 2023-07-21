0ba35708c9977fdaab2a63c8bcfea4dc02a2b1f7
alexm <alex-m@outlook.com>
2023-07-21 13:45:31 -0700

    Add IDs to initial 2 boxes

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
     return (
         <div className="App">
             <BoxList boxList={[
-                {bkgColor: "salmon", width: 200, height: 200},
-                {bkgColor: "red", width: 100, height: 400},
+                {id: 0, bkgColor: "salmon", width: 200, height: 200},
+                {id: 1, bkgColor: "red", width: 100, height: 400},
             ]} />
         </div>
     );
