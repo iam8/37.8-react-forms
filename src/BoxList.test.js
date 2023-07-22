a4ca1b521107806cfa76458fe26f6d4954c231ef
alexm <alex-m@outlook.com>
2023-07-22 12:23:17 -0700

    Bug fix - call asFragment() in expect(); add test for snapshot with no initial boxes rendered

--- src/BoxList.test.js
+++ src/BoxList.test.js
@@ -3,16 +3,23 @@ import BoxList from './BoxList';
 
 
 const testList = [
-    {bkgColor: "salmon", width: 200, height: 200},
+    {bkgColor: "gold", width: 200, height: 200},
     {bkgColor: "blue", width: 500, height: 100},
 ];
+
 
 test("Renders without crashing", () => {
     render(<BoxList boxList={testList} />);
 });
 
 
-test("Matches snapshot for a single box rendered", () => {
-    const {asFragment} = render(<BoxList boxList={testList} />);
-    expect(asFragment).toMatchSnapshot();
+test("Matches snapshot with no initial boxes rendered", () => {
+    const {asFragment} = render(<BoxList />);
+    expect(asFragment()).toMatchSnapshot();
+});
+
+
+test("Matches snapshot with initial boxes rendered", () => {
+    const {asFragment} = render(<BoxList boxList={testList}/>);
+    expect(asFragment()).toMatchSnapshot();
 });
