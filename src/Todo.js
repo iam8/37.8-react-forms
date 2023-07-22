dac401b6c11a273c044536861a02b2afaf617e13
alexm <alex-m@outlook.com>
2023-07-22 14:05:22 -0700

    Add Todo component definition

--- src/Todo.js
+++ src/Todo.js
@@ -1,1 +1,30 @@
 import React from "react";
+
+
+/**
+ * Todo - render a single todo item with the given properties.
+ *
+ * Properties:
+ * - id: a unique ID
+ * - text (string): the text of the todo task
+ * - remove(id): removal function passed down by parent component
+ */
+function Todo({id, text, remove}) {
+
+    /** Remove this box from parent component. */
+    const handleRemove = () => {
+        remove(id);
+    }
+
+    return (
+        <div className="Todo" data-testid="Todo">
+            <span className="Todo-item">
+                {text}
+            </span>
+            <button className="Todo-remove-btn" onClick={handleRemove}>X</button>
+        </div>
+    )
+}
+
+
+export default Todo;
