63c53c011cb406b73b64882f938631d1c75f500c
alexm <alex-m@outlook.com>
2023-07-22 12:33:58 -0700

    Add snapshot test; write dummy box-adding function for form

--- src/NewBoxForm.test.js
+++ src/NewBoxForm.test.js
@@ -2,6 +2,18 @@ import { render, fireEvent } from '@testing-library/react';
 import NewBoxForm from './NewBoxForm';
 
 
+/** Dummy box-adding function to pass in to form. Does nothing. */
+const addBoxTest = () => {
+    return;
+};
+
+
 test("Renders without crashing", () => {
-    render(<NewBoxForm />);
+    render(<NewBoxForm addBox={addBoxTest} />);
+});
+
+
+test("Matches snapshot", () => {
+    const {asFragment} = render(<NewBoxForm addBox={addBoxTest} />);
+    expect(asFragment()).toMatchSnapshot();
 });
