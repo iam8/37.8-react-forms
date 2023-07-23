1b922b099cc832dcb19259772b5322cb4eb265a9
alexm <alex-m@outlook.com>
2023-07-23 13:03:37 -0700

    Add smoke and snapshot tests for NewTodoForm

---
+++ src/NewTodoForm.test.js
@@ -0,0 +1,19 @@
+import { render, fireEvent } from '@testing-library/react';
+import NewTodoForm from './NewTodoForm';
+
+
+/** Dummy todo-adding function to pass in to form. Does nothing. */
+const addTodoTest = () => {
+    return;
+};
+
+
+test("Renders without crashing", () => {
+    render(<NewTodoForm addTodo={addTodoTest} />);
+});
+
+
+test("Matches snapshot", () => {
+    const {asFragment} = render(<NewTodoForm addTodo={addTodoTest} />);
+    expect(asFragment()).toMatchSnapshot();
+});
